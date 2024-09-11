62dbac55c25ac3eb34d75be3f163facb0fe0ab95
Kylsky <38944162+Kylsky@users.noreply.github.com>
2024-09-11 14:29:58 +0800

    update readme

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-docker buildx build --platform linux/amd64 -t linuxdo-oauth2-java:latest --load .
-docker save linuxdo-oauth2-java:latest -o pandora-helper-vue-version.tar
-scp -P 10022 pandora-helper-vue-version.tar yeelo@mysh.yeelo.fun:/home/oauth-test
+docker buildx build --platform linux/amd64 -t pandora_helper_v2:latest --load .
+docker save pandora_helper_v2:latest -o pandora-helper-vue-version.tar
+#scp -P 10022 pandora-helper-vue-version.tar yeelo@mysh.yeelo.fun:/home/oauth-test
 #scp -P 22 pandora-helper.tar root@192.227.138.159:/root/helper-template
