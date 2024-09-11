5c9da096156a23983a737e1edea5d7becd3853d6
Kylsky <38944162+Kylsky@users.noreply.github.com>
2024-09-11 14:22:06 +0800

    Delete build.sh

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build --platform linux/amd64 -t linuxdo-oauth2-java:latest --load .
-docker save linuxdo-oauth2-java:latest -o pandora-helper-vue-version.tar
-scp -P 10022 pandora-helper-vue-version.tar yeelo@mysh.yeelo.fun:/home/oauth-test
-#scp -P 22 pandora-helper.tar root@192.227.138.159:/root/helper-template
