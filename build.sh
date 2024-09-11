04488ccefe572efd56469d6ca35358ebb8b0c95a
Kylsky <38944162+Kylsky@users.noreply.github.com>
2024-09-11 14:30:21 +0800

    修改初始数据

--- build.sh
+++
@@ -1,4 +0,0 @@
-docker buildx build --platform linux/amd64 -t pandora_helper_v2:latest --load .
-docker save pandora_helper_v2:latest -o pandora-helper-vue-version.tar
-#scp -P 10022 pandora-helper-vue-version.tar yeelo@mysh.yeelo.fun:/home/oauth-test
-#scp -P 22 pandora-helper.tar root@192.227.138.159:/root/helper-template
