a9fb6a556bc837c5d2c16129b5f0d74b9d1901f3
Kylsky <38944162+Kylsky@users.noreply.github.com>
2024-09-23 11:12:11 +0800

    新增o1相关模型的统计接口

--- src/main/java/fun/yeelo/oauth/domain/UsageVO.java
+++ src/main/java/fun/yeelo/oauth/domain/UsageVO.java
@@ -4,9 +4,13 @@ import lombok.Data;
 
 @Data
 public class UsageVO {
-    private Integer gpt4o=0;
+    private Integer gpt4o = 0;
 
-    private Integer gpt4=0;
+    private Integer gpt4 = 0;
 
-    private Integer gpt4omini=0;
+    private Integer gpt4omini = 0;
+
+    private Integer o1Preview = 0;
+
+    private Integer o1Mini = 0;
 }
