c42f6acb8c8f174b0d77b7457fc38e92e24a7957
Kylsky <38944162+Kylsky@users.noreply.github.com>
2025-03-04 18:38:48 +0800

    1.mj 支持 linux do oauth 2. update readme

--- src/main/java/fun/yeelo/oauth/controller/OAuthController.java
+++ src/main/java/fun/yeelo/oauth/controller/OAuthController.java
@@ -137,8 +137,10 @@ public class OAuthController {
                     userResBody.put("shareType", "Claude");
                 } else if (state.contains("panel")){
                     userResBody.put("shareType", "panel");
-                }else {
+                }else if (state.contains("pandora")){
                     userResBody.put("shareType", "ChatGPT");
+                }else if (state.contains("midjourney")){
+                    userResBody.put("shareType", "midjourney");
                 }
                 String jsonString = JSON.toJSONString(userResBody, SerializerFeature.PrettyFormat, SerializerFeature.WriteMapNullValue,
                         SerializerFeature.WriteDateUseDateFormat);
