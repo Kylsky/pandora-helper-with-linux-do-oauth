030be251f483332e594fa25aea0e6c78b5e0f69a
Kylsky <38944162+Kylsky@users.noreply.github.com>
2025-03-11 10:37:53 +0800

    优化画图界面展示

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
-docker buildx build --platform linux/amd64 -t kylsky/pandora_helper_v2:latest --load .
-docker buildx build --platform linux/arm64 -t kylsky/pandora_helper_v2_arm:latest --load .
-docker push kylsky/pandora_helper_v2:latest
-docker push kylsky/pandora_helper_v2_arm:latest
+#docker buildx build --platform linux/amd64 -t kylsky/pandora_helper_v2:latest --load .
+#docker buildx build --platform linux/arm64 -t kylsky/pandora_helper_v2_arm:latest --load .
+#docker push kylsky/pandora_helper_v2:latest
+#docker push kylsky/pandora_helper_v2_arm:latest
 
-#docker buildx build --platform linux/amd64 -t kylsky/pandora_helper_v2_preview:latest --load .
-#docker push kylsky/pandora_helper_v2_preview:latest
+docker buildx build --platform linux/amd64 -t kylsky/pandora_helper_v2_preview:latest --load .
+docker push kylsky/pandora_helper_v2_preview:latest
